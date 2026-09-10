fde34be275c8661ef663bbc47db7d4aa5ff45347
ThArGos <thargos@gmail.com>
2026-09-10 17:02:12 +0200

    Meta: Remove original copyright header from test file
    The test file is now mostly new code written for this fork; the GPL
    grant text stays.

--- tests/leman-tests.el
+++ tests/leman-tests.el
@@ -1,9 +1,5 @@
 ;;; leman-tests.el --- Tests for Leman.el                  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2023  Free Software Foundation, Inc.
-
-;; Author: Adam Porter <adam@alphapapa.net>
-
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
